96fc7e854b494d66f8b9b11056494b524d842d25
Abigail Marsh <amarsh1@macalester.edu>
2020-03-11 13:42:36 -0500

    In-class demo 2020-3-11

--- src/marsh/mutableProblems/BadActor.java
+++ src/marsh/mutableProblems/BadActor.java
@@ -11,5 +11,15 @@ public class BadActor {
         vc.resetName("Pippa");
         System.out.println(vc.getHistory());
 
+        // Strings are not mutable
+        String temp = vc.getName();
+        temp = temp + 'o';
+        System.out.println(vc.getHistory());
+
+        // Oh no! The List is mutable
+        vc.checkHistory("Bad Joe");
+        vc.getHistory().add("Bad Joe");
+        vc.checkHistory("Bad Joe");
+
     }
 }
--- src/marsh/mutableProblems/VulnerableClass.java
+++ src/marsh/mutableProblems/VulnerableClass.java
@@ -32,6 +32,6 @@ public class VulnerableClass {
     }
 
     public List<String> getHistory() {
-        return history;
+        return this.history;
     }
 }
